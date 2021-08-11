cc22d8e284dfc7e4a85ce7225deb989afd9b9f97
cam2332 <kam2332@gmail.com>
2021-08-11 20:11:46 +0200

    Update index

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,86 @@
 import * as fs from 'fs'
 import * as rd from 'readline'
+import { TokenType } from './TokenType'
+import Token from './Token'
+import util from 'util'
 import { tokenize } from './Lexer'
 import { mainParse } from './Parser'
 
-const reader = rd.createInterface(
-  fs.createReadStream('./resources/testfile.kr')
-)
+const args = process.argv.slice(2)
 
-const data = tokenize(reader)
+const inputFilePath = args[0]
+
+let inputFileName = inputFilePath
+  .substring(0, inputFilePath.lastIndexOf('.'))
+  .substring(inputFilePath.lastIndexOf('/') + 1)
+
+let inputFileDir = inputFilePath
+  .substring(0, inputFilePath.lastIndexOf('.'))
+  .substring(0, inputFilePath.lastIndexOf('/'))
+
+const reader = rd.createInterface(fs.createReadStream(inputFilePath))
+
+const tokens = tokenize(reader)
 
 reader.on('close', () => {
-  printTokens(data)
+  try {
+    const stream = fs.createWriteStream(
+      inputFileDir + inputFileName + '-tokens' + '.json'
+    )
+    stream.write('[')
+    tokens.forEach((token, index) => {
+      stream.write(
+        JSON.stringify(
+          {
+            line: token.line,
+            column: token.column,
+            type: TokenType[token.type],
+            value: token.value,
+          },
+          undefined,
+          2
+        )
+      )
+      if (index < tokens.length - 1) {
+        stream.write(',')
+      }
+    })
+    stream.write(']')
+    stream.end()
+  } catch (err) {
+    console.log(err)
+  }
 
-  const program = mainParse(data)
+  printTokens(tokens)
+
+  const program = mainParse(tokens)
+
+  try {
+    fs.writeFileSync(
+      inputFileDir + inputFileName + '-parseTree' + '.json',
+      JSON.stringify(program, undefined, 2)
+    )
+  } catch (err) {
+    console.log(err)
+  }
 
   console.log('parser\n', JSON.stringify(program, null, 2))
 })
+
+function printTokens(data: Token[]): void {
+  data.forEach((token) => {
+    console.log(
+      util.inspect(
+        {
+          line: token.line,
+          column: token.column,
+          type: TokenType[token.type],
+          value: token.value,
+        },
+        false,
+        1,
+        true
+      )
+    )
+  })
+}
