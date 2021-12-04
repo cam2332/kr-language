b073a13a9f48d6b71eaea9f89fd39bceae844289
cam2332 <kam2332@gmail.com>
2021-12-04 13:57:48 +0100

    Set missing $type property

--- src/AST/BinaryExpression.ts
+++ src/AST/BinaryExpression.ts
@@ -3,6 +3,7 @@ import Node from './Node'
 export default class BinaryExpression extends Node {
   constructor(public left: Node, public operator: string, public right: Node) {
     super()
+    this.$type = 'BinaryExpression'
   }
 
   public toJSON(): any {
--- src/AST/BlockStatement.ts
+++ src/AST/BlockStatement.ts
@@ -3,6 +3,7 @@ import Node from './Node'
 export default class BlockStatement extends Node {
   constructor(public body: Node[] = []) {
     super()
+    this.$type = 'BlockStatement'
   }
 
   public toJSON(): any {
