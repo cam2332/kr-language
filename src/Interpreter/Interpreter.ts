b0a4f514497dd09e60e7098c62066c4d32e752bf
cam2332 <kam2332@gmail.com>
2021-12-08 15:24:10 +0100

    Add Interpreter class

---
+++ src/Interpreter/Interpreter.ts
@@ -0,0 +1,23 @@
+import Environment from './Environment'
+import Node from '../AST/Node'
+
+export default class Interpreter {
+  readonly globals: Environment = new Environment()
+  private environment: Environment = this.globals
+  constructor() {
+  }
+
+  interpret(nodes: Node[]): void {
+    try {
+      nodes.forEach((node) => this.execute(node))
+    } catch (error) {
+      console.error('e', error)
+    }
+  }
+
+  private evaluate(node: Node): Object {
+  }
+
+  private execute(node: Node): void {
+  }
+}
