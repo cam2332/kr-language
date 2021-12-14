42b4a0a868cf29c8becc8098dd301e6391625c18
cam2332 <kam2332@gmail.com>
2021-12-14 15:23:55 +0100

    Add method initMinusOne that return Position with all values of -1

--- src/types/Position.ts
+++ src/types/Position.ts
@@ -2,3 +2,13 @@ export default interface Position {
   start: { line: number; column: number }
   end: { line: number; column: number }
 }
+
+/**
+ * @returns default values (-1) for  position
+ */
+export function initMinusOne(): Position {
+  return {
+    start: { line: -1, column: -1 },
+    end: { line: -1, column: -1 },
+  }
+}
