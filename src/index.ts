64510b3c33bdcee68d67133a86c9f3a67e7d3bcf
cam2332 <kam2332@gmail.com>
2021-12-21 12:23:02 +0100

    Use Kr class in index file

--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,10 @@
-import * as fs from 'fs'
-import * as rd from 'readline'
-import { TokenType } from './types/TokenType'
-import { tokenize } from './Lexer'
-import Parser from './Parser/Parser'
-import Interpreter from './Interpreter/Interpreter'
+import Kr from './Kr'
 
 const args = process.argv.slice(2)
 
 const inputFilePath = args[0]
+if (args[1] && args[1] === 'debugMode') {
+  Kr.debugMode = true
+}
 
-let inputFileName = inputFilePath
-  .substring(0, inputFilePath.lastIndexOf('.'))
-  .substring(inputFilePath.lastIndexOf('/') + 1)
-
-let inputFileDir = inputFilePath
-  .substring(0, inputFilePath.lastIndexOf('.'))
-  .substring(0, inputFilePath.lastIndexOf('/'))
-
-const reader = rd.createInterface(fs.createReadStream(inputFilePath))
-
-const tokens = tokenize(reader)
-
-reader.on('close', () => {
-  try {
-    const stream = fs.createWriteStream(
-      inputFileDir + inputFileName + '-tokens' + '.json'
-    )
-    stream.write('[')
-    tokens.forEach((token, index) => {
-      stream.write(
-        JSON.stringify(
-          {
-            position: token.position,
-            type: TokenType[token.type],
-            value: token.value,
-          },
-          undefined,
-          2
-        )
-      )
-      if (index < tokens.length - 1) {
-        stream.write(',')
-      }
-    })
-    stream.write(']')
-    stream.end()
-  } catch (err) {
-    console.log(err)
-  }
-
-  const parser = new Parser(tokens)
-  const program = parser.parse()
-
-  const interpreter: Interpreter = new Interpreter()
-  interpreter.interpret(program.body)
-
-  try {
-    fs.writeFileSync(
-      inputFileDir + inputFileName + '-parseTree' + '.json',
-      JSON.stringify(program, undefined, 2)
-    )
-  } catch (err) {
-    console.log(err)
-  }
-})
+const kr: Kr = new Kr(inputFilePath)
