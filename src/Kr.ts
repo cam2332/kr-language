40fe40fe1776936df5c0c3e2a717a98aa8ee0eaa
cam2332 <kam2332@gmail.com>
2021-12-21 17:05:49 +0100

    Add printError method

--- src/Kr.ts
+++ src/Kr.ts
@@ -4,6 +4,7 @@ import Program from './AST/Program'
 import Interpreter from './Interpreter/Interpreter'
 import { tokenize } from './Lexer'
 import Parser from './Parser/Parser'
+import KrError from './types/KrError'
 import Token from './types/Token'
 import { TokenType } from './types/TokenType'
 
@@ -19,11 +20,11 @@ export default class Kr {
   constructor(path: string) {
     Kr.fileName = path
       .substring(0, path.lastIndexOf('.'))
-      .substring(path.lastIndexOf('/') + 1)
+      .substring(path.lastIndexOf('\\') + 1)
 
     Kr.fileDir = path
       .substring(0, path.lastIndexOf('.'))
-      .substring(0, path.lastIndexOf('/'))
+      .substring(0, path.lastIndexOf('\\'))
     Kr.runFile(path)
   }
 
@@ -48,6 +49,60 @@ export default class Kr {
     if (this.hadError) return
 
     this.interpreter.interpret(program.body)
+  }
+
+  public static printError(error: KrError) {
+    const errorLine: string =
+      Kr.sourceFile.split('\n')[error.position.start.line - 1]
+
+    const decoratedErrorLine =
+      error.position.start.line >= 0
+        ? '\n' +
+          '\x1b[30m\x1b[47m' +
+          error.position.start.line +
+          '\x1b[37m\x1b[40m' +
+          errorLine +
+          '\n' +
+          '\x1b[30m\x1b[47m' +
+          ' '.repeat(error.position.start.line.toString().length) +
+          '\x1b[37m\x1b[40m' +
+          ' '.repeat(error.position.start.column) +
+          '\x1b[31m' +
+          '~'.repeat(
+            error.position.start.line === error.position.end.line
+              ? error.position.end.column - error.position.start.column
+              : errorLine.length - error.position.start.column
+          ) +
+          '\x1b[37m'
+        : ''
+
+    console.error(
+      '\x1b[31m' +
+        error.name +
+        '\x1b[38;2;126;126;126m' +
+        ' ' +
+        'errorCode' +
+        '\x1b[37m' +
+        ' in ' +
+        '\x1b[38;2;47;176;257m' +
+        Kr.fileName +
+        '.kr' +
+        '\x1b[37m' +
+        ' at line ' +
+        '\x1b[38;5;220m' +
+        error.position.start.line +
+        '\x1b[37m' +
+        ', column ' +
+        '\x1b[38;5;220m' +
+        error.position.start.column +
+        '\x1b[37m' +
+        '\nMessage: ' +
+        '\x1b[38;2;211;255;255m' +
+        error.message +
+        '\x1b[37m' +
+        '\n' +
+        decoratedErrorLine
+    )
   }
 
   private static writeTokensToFile(tokens: Token[]): void {
