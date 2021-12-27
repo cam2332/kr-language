63bb0094ba3ab03f9c057f907ea8c2cd115d1c63
cam2332 <kam2332@gmail.com>
2021-12-27 20:02:59 +0100

    Change ObjectExpression property 'properties' type from Node[] to ObjectProperty[]

--- src/AST/ObjectExpression.ts
+++ src/AST/ObjectExpression.ts
@@ -1,8 +1,9 @@
 import Position from '../types/Position'
 import Node from './Node'
+import ObjectProperty from './ObjectProperty'
 
 export default class ObjectExpression extends Node {
-  constructor(public properties: Node[] = [], position: Position) {
+  constructor(public properties: ObjectProperty[] = [], position: Position) {
     super('ObjectExpression', position)
   }
 
